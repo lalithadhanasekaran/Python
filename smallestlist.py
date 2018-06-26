dc697ff3fbc6914959bd39f68936e0c79cc2db87
lalithgadhanasekaran <lalitha.d@vvdntech.in>
2018-06-26 12:21:47 +0530

    smallest number

---
+++ smallestlist.py
@@ -0,0 +1,11 @@
+numbers=[1,9,5,-5,0,4,8,-3]
+a=min(numbers)
+b=max(numbers)
+print "minimum number is: ",a
+print "maximum number is: ",b
+things=["honey","chair","happy","joke"]
+a=min(things)
+b=max(things)
+print "minimum things is: ",a
+print "maximum things is: ",b
+
